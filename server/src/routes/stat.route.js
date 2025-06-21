8bd9167d97bc93e92cc326b737467c83ea13f2a9
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-21 19:12:09 +0530

    Implement all other controllers and routes

--- server/src/routes/stat.route.js
+++ server/src/routes/stat.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getStatistics } from "../controllers/stat.controller.js";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Stat route with GET method");
-});
+router.get("/", protectRoute, requireAdmin, getStatistics);
 
 export default router;
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { getAllUsers } from "../controllers/user.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("User route with GET method");
-});
+router.get("/", protectRoute, getAllUsers);
+
+// todo: getMessages
 
 export default router;
