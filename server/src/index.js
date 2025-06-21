51c578f1e27d4f7d80ce5c1d5ccbb41249248d4b
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-21 14:42:14 +0530

    Configure Cloudinary &  Add fileUplaod and error Handler middleware,

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
+import fileUpload from "express-fileupload";
 import { clerkMiddleware } from "@clerk/express";
 
 import connectDB from "./lib/db.js";
@@ -8,11 +10,12 @@ import connectDB from "./lib/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
-import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
+import albumRoutes from "./routes/album.route.js";
 import adminRoutes from "./routes/admin.route.js";
 
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 5432;
 
@@ -20,6 +23,16 @@ app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10 MB max file size
+    },
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
@@ -27,6 +40,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/stats", statRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/albums", albumRoutes);
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal server error"
+        : err.message,
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend of the spotify app");
