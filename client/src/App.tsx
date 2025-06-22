7068c01ebf108c09b392b905792dfb1c6077699a
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-22 16:57:25 +0530

    Add the Main Layout & Left Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 import Home from "./pages/Home";
+import Chat from "./pages/Chat";
 import AuthCallback from "./pages/AuthCallback";
+
+import MainLayout from "./layout/MainLayout";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route
           path="/sso-callback"
           element={
@@ -18,6 +20,10 @@ const App = () => {
           }
         />
         <Route path="/auth-callback" element={<AuthCallback />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
       </Routes>
     </>
   );
