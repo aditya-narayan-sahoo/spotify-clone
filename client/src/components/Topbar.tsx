f5f4cc05c81b1db3049b8359bf5e29ad2f532ce9
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-28 09:34:53 +0530

    Create the home page

--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -1,27 +1,36 @@
 import { Link } from "react-router-dom";
 import { LayoutDashboardIcon } from "lucide-react";
-import { SignedOut, SignedIn, SignOutButton } from "@clerk/clerk-react";
 
+import { cn } from "@/lib/utils";
+import { useAuthStore } from "@/store/useAuthStore";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+
+import { buttonVariants } from "./ui/button";
 import SignInOAuthButtons from "./SignInOAuthButtons";
 
 const Topbar = () => {
-  const isAdmin = false;
+  const { isAdmin } = useAuthStore();
+
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-      <div className="flex gap-2 items-center">Spotify</div>
+      <div className="flex gap-2 items-center">
+        <img src="/spotify.png" alt="spotify" className="size-8" />
+        Spotify
+      </div>
       <div className="flex items-center gap-4">
         {isAdmin && (
-          <Link to={"/admin"}>
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
             <LayoutDashboardIcon className="size-4 mr-2" />
             Admin Dashboard
           </Link>
         )}
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
         <SignedOut>
           <SignInOAuthButtons />
         </SignedOut>
+        <UserButton />
       </div>
     </div>
   );
