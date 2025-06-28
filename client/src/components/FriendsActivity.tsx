8373b68560ae4cbe4cddc7d6d1573eb4bac353dd
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-28 07:24:39 +0530

    Create Friends Activity component

---
+++ client/src/components/FriendsActivity.tsx
@@ -0,0 +1,82 @@
+import { useEffect } from "react";
+import { Music, Users } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+import { ScrollArea } from "./ui/scroll-area";
+import LoginPrompt from "@/skeletons/LoginPrompt";
+import { useChatStore } from "@/store/useChatStore";
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const FriendsActivity = () => {
+  const isPlaying = true;
+  const { user } = useUser();
+  const { users, fetchUsers } = useChatStore();
+  useEffect(() => {
+    if (user) fetchUsers();
+  }, [fetchUsers, user]);
+  return (
+    <div className="h-full bg-zinc-900 rounded-lg flex flex-col">
+      <div className="p-4 flex justify-between items-center border-b border-zinc-800">
+        <div className="flex items-center gap-2">
+          <Users className="size-5 shrink-0" />
+          <h2 className="font-semibold">What they're listening to</h2>
+        </div>
+      </div>
+
+      {!user && <LoginPrompt />}
+
+      <ScrollArea className="flex-1">
+        <div className="p-4 space-y-4">
+          {users.map((user) => {
+            return (
+              <div
+                key={user._id}
+                className="cursor-pointer hover:bg-zinc-800/50 p-3 rounded-md transition-colors group"
+              >
+                <div className="flex items-start gap-3">
+                  <div className="relative">
+                    <Avatar className="size-10 border border-zinc-800">
+                      <AvatarImage src={user.imageUrl} alt={user.fullName} />
+                      <AvatarFallback>{user.fullName[0]}</AvatarFallback>
+                    </Avatar>
+                    <div
+                      className={`absolute bottom-0 right-0 h-3 w-3 rounded-full border-2 border-zinc-900"}
+												`}
+                      aria-hidden="true"
+                    />
+                  </div>
+
+                  <div className="flex-1 min-w-0">
+                    <div className="flex items-center gap-2">
+                      <span className="font-medium text-sm text-white">
+                        {user.fullName}
+                      </span>
+                      {isPlaying && (
+                        <Music className="size-3.5 text-emerald-400 shrink-0" />
+                      )}
+                    </div>
+
+                    {isPlaying ? (
+                      <div className="mt-1">
+                        <div className="mt-1 text-sm text-white font-medium truncate">
+                          Reminder
+                        </div>
+                        <div className="text-xs text-zinc-400 truncate">
+                          by The Weekend
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="mt-1 text-xs text-zinc-400">Idle</div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default FriendsActivity;
--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -6,6 +6,7 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import LeftSidebar from "@/components/LeftSidebar";
+import FriendsActivity from "@/components/FriendsActivity";
 
 const MainLayout = () => {
   const isMobile = false;
@@ -37,7 +38,7 @@ const MainLayout = () => {
           maxSize={25}
           collapsedSize={0}
         >
-          Friends Activity
+          <FriendsActivity />
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
