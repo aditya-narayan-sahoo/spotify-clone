9ab0c85ab2c46fb2ef4d63f67340f7e447dc90e1
Aditya Narayan Sahoo <adityasahooo246@gmail.com>
2025-06-28 12:28:19 +0530

    Implemen the play song & queue functionality

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,21 +1,35 @@
-import { Outlet } from "react-router-dom";
-
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import LeftSidebar from "@/components/LeftSidebar";
+import AudioPlayer from "@/components/AudioPlayer";
 import FriendsActivity from "@/components/FriendsActivity";
 
 const MainLayout = () => {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
   return (
     <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup
         direction="horizontal"
         className="flex-1 flex h-full overflow-hidden p-2"
       >
+        <AudioPlayer />
+        {/* left sidebar */}
         <ResizablePanel
           defaultSize={20}
           minSize={isMobile ? 0 : 10}
@@ -23,6 +37,7 @@ const MainLayout = () => {
         >
           <LeftSidebar />
         </ResizablePanel>
+
         <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
         {/* Main content */}
@@ -30,19 +45,23 @@ const MainLayout = () => {
           <Outlet />
         </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-        {/* Right sidebar */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-        >
-          <FriendsActivity />
-        </ResizablePanel>
+        {!isMobile && (
+          <>
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+            {/* right sidebar */}
+            <ResizablePanel
+              defaultSize={20}
+              minSize={0}
+              maxSize={25}
+              collapsedSize={0}
+            >
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
     </div>
   );
 };
-
 export default MainLayout;
--- client/src/pages/Album.tsx
+++ client/src/pages/Album.tsx
@@ -1,31 +1,54 @@
 import { useEffect } from "react";
-import { Clock, Play } from "lucide-react";
-import { useParams } from "react-router-dom";
-
 import { formatDuration } from "@/utils";
-
+import { useParams } from "react-router-dom";
+import { Clock, Pause, Play } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
-
 import { useMusicStore } from "@/store/useMusicStore";
+import { usePlayerStore } from "@/store/usePlayerStore";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Album = () => {
   const { albumId } = useParams();
   const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+  const { currentSong, isPlaying, playAlbum, togglePlay } = usePlayerStore();
 
   useEffect(() => {
     if (albumId) fetchAlbumById(albumId);
-  }, [albumId, fetchAlbumById]);
+  }, [fetchAlbumById, albumId]);
+
   if (isLoading) return null;
-  if (!currentAlbum) return <div>Album not found</div>;
+
+  const handlePlayAlbum = () => {
+    if (!currentAlbum) return;
+
+    const isCurrentAlbumPlaying = currentAlbum?.songs.some(
+      (song) => song._id === currentSong?._id
+    );
+    if (isCurrentAlbumPlaying) togglePlay();
+    else {
+      // start playing the album from the beginning
+      playAlbum(currentAlbum?.songs, 0);
+    }
+  };
+
+  const handlePlaySong = (index: number) => {
+    if (!currentAlbum) return;
+    playAlbum(currentAlbum?.songs, index);
+  };
+
   return (
     <div className="h-full">
       <ScrollArea className="h-full rounded-md">
+        {/* Main Content */}
         <div className="relative min-h-full">
+          {/* bg gradient */}
           <div
-            className="absolute inset-0 bg-gradient-to-b from-[#5038a0]/80 via-zinc-900/80 to-zinc-900 pointer-events-none"
+            className="absolute inset-0 bg-gradient-to-b from-[#5038a0]/80 via-zinc-900/80
+					 to-zinc-900 pointer-events-none"
             aria-hidden="true"
           />
+
+          {/* Content */}
           <div className="relative z-10">
             <div className="flex p-6 gap-6 pb-8">
               <img
@@ -42,65 +65,98 @@ const Album = () => {
                   <span className="font-medium text-white">
                     {currentAlbum?.artist}
                   </span>
-                  <span>• {currentAlbum?.songs.length}</span>
+                  <span>• {currentAlbum?.songs.length} songs</span>
                   <span>• {currentAlbum?.releaseYear}</span>
                 </div>
               </div>
             </div>
-            {/* controls */}
+
+            {/* play button */}
             <div className="px-6 pb-4 flex items-center gap-6">
               <Button
-                size={"icon"}
-                className="size-14 rounded-full bg-green-500 hover:bg-green-400 hover:scale-105 transition-all"
+                onClick={handlePlayAlbum}
+                size="icon"
+                className="size-14 rounded-full bg-green-500 hover:bg-green-400 
+                hover:scale-105 transition-all"
               >
-                <Play className="size-7 text-black" />
+                {isPlaying &&
+                currentAlbum?.songs.some(
+                  (song) => song._id === currentSong?._id
+                ) ? (
+                  <Pause className="size-7 text-black" />
+                ) : (
+                  <Play className="size-7 text-black" />
+                )}
               </Button>
             </div>
-            {/* Table section */}
+
+            {/* Table Section */}
             <div className="bg-black/20 backdrop-blur-sm">
-              <section className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm text-zinc-400 border-b border-white/5">
+              {/* table header */}
+              <div
+                className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm 
+            text-zinc-400 border-b border-white/5"
+              >
                 <div>#</div>
                 <div>Title</div>
                 <div>Released Date</div>
                 <div>
                   <Clock className="size-4" />
                 </div>
-              </section>
-              {/* Songs list */}
-              <section className="px-6">
+              </div>
+
+              {/* songs list */}
+
+              <div className="px-6">
                 <div className="space-y-2 py-4">
-                  {currentAlbum?.songs.map((song, index) => (
-                    <div
-                      key={song._id}
-                      className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer`}
-                    >
-                      <div className="flex items-center justify-center">
-                        <span className="group-hover:hidden">{index + 1}</span>
-                        <Play className="size-4 hidden group-hover:block" />
-                      </div>
-                      <div className="flex items-center gap-3">
-                        <img
-                          src={song.imageUrl}
-                          alt={song.title}
-                          className="size-10"
-                        />
-                        <div>
-                          <div className={`font-medium text-white`}>
-                            {song.title}
+                  {currentAlbum?.songs.map((song, index) => {
+                    const isCurrentSong = currentSong?._id === song._id;
+                    return (
+                      <div
+                        key={song._id}
+                        onClick={() => handlePlaySong(index)}
+                        className={`grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
+                      text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer
+                      `}
+                      >
+                        <div className="flex items-center justify-center">
+                          {isCurrentSong && isPlaying ? (
+                            <div className="size-4 text-green-500">♫</div>
+                          ) : (
+                            <span className="group-hover:hidden">
+                              {index + 1}
+                            </span>
+                          )}
+                          {!isCurrentSong && (
+                            <Play className="size-4 hidden group-hover:block" />
+                          )}
+                        </div>
+
+                        <div className="flex items-center gap-3">
+                          <img
+                            src={song.imageUrl}
+                            alt={song.title}
+                            className="size-10"
+                          />
+
+                          <div>
+                            <div className={`font-medium text-white`}>
+                              {song.title}
+                            </div>
+                            <div>{song.artist}</div>
                           </div>
-                          <div>{song.artist}</div>
+                        </div>
+                        <div className="flex items-center">
+                          {song.createdAt.split("T")[0]}
+                        </div>
+                        <div className="flex items-center">
+                          {formatDuration(song.duration)}
                         </div>
                       </div>
-                      <div className="flex items-center">
-                        {song.createdAt.split("T")[0]}
-                      </div>
-                      <div className="flex items-center">
-                        {formatDuration(song.duration)}
-                      </div>
-                    </div>
-                  ))}
+                    );
+                  })}
                 </div>
-              </section>
+              </div>
             </div>
           </div>
         </div>
@@ -108,5 +164,4 @@ const Album = () => {
     </div>
   );
 };
-
 export default Album;
